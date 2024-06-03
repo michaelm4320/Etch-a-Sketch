d68a6d31092a149432ebb5cc1d7c8dff4b6b18d2
Michael <michaelmartinez4320@yahoo.com>
2024-06-02 22:57:08 -0400

    Add resize option but not fully completed

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ grid.addEventListener("mouseover", function (e) {
     e.target.style.background = "green";
 });
 
+let number = 0;
+const btn = document.querySelector("button");
+btn.addEventListener("click", function () {
+    number = prompt("Please enter your new grid size.");
+    while (number > 100 || number < 0) {
+        number = prompt("Invalid value, value should be 0 - 100");
+    }
+    grid.remove();
+    const gridTwo = document.querySelector(".container-two");
+    for (let i = 0; i < number; i++) {
+        for (let j = 0; j < number; j++) {
+            let cell = document.createElement("div");
+            cell.classList.add("cell");
+            gridTwo.appendChild(cell);
+        }
+    }
+});
+
